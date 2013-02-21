cba1c093990026baaeec1c1758d7108b5483bd4e
Paul David Mena <pmena@constantcontact.com>
2013-02-21 14:57:41 -0500

    add connect_to_vpn.sh script

---
+++ connect_to_vpn.sh
@@ -0,0 +1,6 @@
+#!/bin/sh
+
+cd /home/pablo/.juniper_networks/network_connect
+
+# ./juniperncprompt.py -r "Active Directory Users" vpn.constantcontact.com
+./juniperncprompt.py vpn.constantcontact.com
