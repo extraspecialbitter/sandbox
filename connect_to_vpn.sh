3231feefd014efa218ec5379d1d79a786e0e4b5e
Paul David Mena <pmena@constantcontact.com>
2013-02-26 13:12:45 -0500

    add comments to connect_vpn.sh script

--- connect_to_vpn.sh
+++ connect_to_vpn.sh
@@ -2,5 +2,10 @@
 
 cd /home/pablo/.juniper_networks/network_connect
 
+#
+# Use the "-r" option when logging in remotely
+# In other words, uncomment the first invocation of juniperncprompt.py
+# and comment out the second
+#
 # ./juniperncprompt.py -r "Active Directory Users" vpn.constantcontact.com
 ./juniperncprompt.py vpn.constantcontact.com
