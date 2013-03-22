65ae80b81dc6341fba76014e14fb5fa3416b3265
Paul David Mena <pmena@constantcontact.com>
2013-03-22 09:42:22 -0400

    change to force remove

--- promoted_apps.sh
+++ promoted_apps.sh
@@ -5,11 +5,11 @@ export search_text=`date +%Y%m%d`
 rm -f matches.txt
 rm -f results.txt
 
-./search_for_recent_baselines.rb 20130319
+./search_for_recent_baselines.rb 20130321
 
 for i in `cat results.txt | cut -d',' -f1 | cut -d'"' -f2 | sort -u`
 do 
-  grep $i results.txt | grep 20130319 >> matches.txt
+  grep $i results.txt | grep 20130321 >> matches.txt
 done
 
 # sort -u matches.txt | wc -l
