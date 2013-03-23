10f0c60d48c93067ff7bbb0e189dafd74e6b0fe9
Paul David Mena <paul@extraspecialbitter.com>
2013-03-23 10:22:05 -0400

    search the test archive

---
+++ haiku_search_archive_test_to_html.rb
@@ -0,0 +1,19 @@
+#!/usr/bin/ruby
+
+require 'mysql'
+
+begin
+    con = Mysql.new 'localhost', 'root', 'menagerie', 'haiku_archive'
+
+    rs = con.query("SELECT * FROM archive_test WHERE haiku_text LIKE '%#{ARGV[0]}%'")
+    n_rows = rs.num_rows
+#   puts n_rows    
+
+    n_rows.times do
+        begin
+            file = File.open("results.html", "a")
+            file.write(rs.fetch_row)
+            file.puts "<br>"
+        end
+    end
+end
