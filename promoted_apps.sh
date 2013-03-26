d8a49fc72ecab4c1a5588caea44b529ebc932365
Paul David Mena <pmena@constantcontact.com>
2013-03-26 09:45:10 -0400

    will need to extract 2013 haiku differently

--- promoted_apps.sh
+++ promoted_apps.sh
@@ -5,11 +5,11 @@ export search_text=`date +%Y%m%d`
 rm -f matches.txt
 rm -f results.txt
 
-./search_for_recent_baselines.rb 20130325
+./search_for_recent_baselines.rb 20130326
 
 for i in `cat results.txt | cut -d',' -f1 | cut -d'"' -f2 | sort -u`
 do 
-  grep $i results.txt | grep 20130325 >> matches.txt
+  grep $i results.txt | grep 20130326 >> matches.txt
 done
 
 # sort -u matches.txt | wc -l
