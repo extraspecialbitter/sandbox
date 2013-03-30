5601c0ec622301423f495e857501a8837326d812
Paul David Mena <paul@extraspecialbitter.com>
2013-03-30 11:18:14 -0400

    add exception for ChopChop uglies

--- process_archive.sh
+++ process_archive.sh
@@ -5,6 +5,7 @@ rm -f ./archive.html
 for i in `ls -1 /export/www/html/haikupoet/archive/[01]???.html`
 do
   ./extract_haiku_2013.rb ${i}
-  ./insert_haiku_from_file_into_table.rb snippet.txt
+  sed  '/^Received\ on/,$d' snippet.txt > qwert.txt
+  ./insert_haiku_from_file_into_table.rb qwert.txt
 done
 ./haiku_to_html.rb
