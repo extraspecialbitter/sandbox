b106b79bfceeb6fb91d3b0b2601e906460fa6591
Paul David Mena <paul@extraspecialbitter.com>
2013-03-31 11:57:20 -0400

    add another exclusion for 2012

--- process_archive_2012.sh
+++ process_archive_2012.sh
@@ -6,6 +6,7 @@ for i in `ls -1 /export/www/html/haikupoet/archive_2012/[01]???.html`
 do
   ./extract_haiku.rb ${i}
   sed  '/^Received\ on/,$d' snippet.txt > qwert.txt
+  sed  '/^Paul\ David\ Mena/,$d' snippet.txt > qwert.txt
   ./insert_haiku_from_file_into_table_2012.rb qwert.txt
 done
 ./haiku_to_html_2012.rb
