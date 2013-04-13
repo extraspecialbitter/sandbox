e8518286ff686ee50ae60a4dec4dc39829235f02
Paul David Mena <paul@extraspecialbitter.com>
2013-04-13 10:25:21 -0400

    create search of 2013 archive

--- haiku_search_archive_2012_to_html.rb
+++ haiku_search_archive_2012_to_html.rb
@@ -5,7 +5,7 @@ require 'mysql'
 begin
     con = Mysql.new 'localhost', 'root', 'menagerie', 'haiku_archive'
 
-    rs = con.query("SELECT * FROM archive_2013 WHERE haiku_text LIKE '%#{ARGV[0]}%'")
+    rs = con.query("SELECT * FROM archive_2012 WHERE haiku_text LIKE '%#{ARGV[0]}%'")
     n_rows = rs.num_rows
 #   puts n_rows    
 
