5c0bb0de8a890b9c19befdfa86ae0be9d249ba57
Paul David Mena <paul@extraspecialbitter.com>
2013-04-28 18:53:21 -0400

    more attempts to process 2011 haiku

--- process_archive_2011.sh
+++ process_archive_2011.sh
@@ -6,7 +6,7 @@ for i in `ls -1 /export/www/html/haikupoet/archive_2011/[01]???.html`
 do
   ./extract_haiku_2011.rb ${i}
   sed  '/^Received\ on/,$d' snippet.txt > qwert.txt
-  sed  '/^Paul\ David\ Mena/,$d' snippet.txt > qwert.txt
+# sed  '/^Paul\ David\ Mena/,$d' snippet.txt > qwert.txt
   ./insert_haiku_from_file_into_table_2011.rb qwert.txt
 done
 ./haiku_to_html_2011.rb
