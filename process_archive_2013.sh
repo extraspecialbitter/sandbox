d193a380086a7c15b4cdfe09fd7183638f9b8fae
Paul David Mena <pmena@constantcontact.com>
2013-05-14 15:56:43 -0400

    update promoted apps

--- process_archive_2013.sh
+++ process_archive_2013.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 
 rm -f ./archive.html
-./drop_table.rb
+./drop_table_2013.rb
 for i in `ls -1 /export/www/html/haikupoet/archive/[01]???.html`
 do
   ./extract_haiku_2013.rb ${i}
   sed  '/^Received\ on/,$d' snippet.txt > qwert.txt
-  ./insert_haiku_from_file_into_table.rb qwert.txt
+  ./insert_haiku_from_file_into_table_2013.rb qwert.txt
 done
-./haiku_to_html.rb
+./haiku_to_html_2013.rb
