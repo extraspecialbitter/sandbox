b8ebe55247de24c8d8566245bd6424bbc9b0e92e
Paul David Mena <paul@extraspecialbitter.com>
2013-05-19 09:58:48 -0400

    correct year

--- haiku_search.rb
+++ haiku_search.rb
@@ -5,7 +5,7 @@ require 'mysql'
 begin
     con = Mysql.new 'localhost', 'root', 'menagerie', 'haiku_archive'
 
-    rs = con.query("SELECT * FROM archive_2013 WHERE haiku_text LIKE '%#{ARGV[0]}%' UNION
+    rs = con.query("SELECT * FROM archive_2010 WHERE haiku_text LIKE '%#{ARGV[0]}%' UNION
                     SELECT * FROM archive_2011 WHERE haiku_text LIKE '%#{ARGV[0]}%' UNION
                     SELECT * FROM archive_2012 WHERE haiku_text LIKE '%#{ARGV[0]}%' UNION
                     SELECT * FROM archive_2013 WHERE haiku_text LIKE '%#{ARGV[0]}%'")
