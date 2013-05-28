eb385a544749b35ed618a27bcf8d8652a3aae1ab
Paul David Mena <pmena@constantcontact.com>
2013-05-28 10:24:04 -0400

    add processing for 2001

---
+++ process_archive_2001.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+rm -f ./archive_2001.html
+./drop_table_2001.rb
+for i in `ls -1 /export/haiku_search/archive_2001/[01]???.html`
+do
+  ./extract_haiku.rb ${i}
+  cp snippet.txt qwert.txt
+  sed  '/^Received\ on/,$d' snippet.txt > qwert.txt
+  sed  '/^Paul\ David\ Mena/,$d' qwert.txt > snippet.txt
+  sed  '/^========================/,$d' snippet.txt > qwert.txt
+  sed  '/^------------------------/,$d' qwert.txt > snippet.txt 
+  sed  '/^________________________/,$d' snippet.txt > qwert.txt
+  ./insert_haiku_from_file_into_table_2001.rb qwert.txt
+done
+./haiku_to_html_2001.rb
