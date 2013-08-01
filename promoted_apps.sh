edee5e81c30c37040852efa5869ad210c900ff62
Paul David Mena <pmena@constantcontact.com>
2013-08-01 15:53:33 -0400

    update promoted apps

--- promoted_apps.sh
+++ promoted_apps.sh
@@ -5,11 +5,11 @@ export search_text=`date +%Y%m%d`
 rm -f matches.txt
 rm -f results.txt
 
-./search_for_recent_baselines.rb 20130731
+./search_for_recent_baselines.rb 20130801
 
 for i in `cat results.txt | cut -d',' -f1 | cut -d'"' -f2 | sort -u`
 do 
-  grep $i results.txt | grep 20130731 >> matches.txt
+  grep $i results.txt | grep 20130801 >> matches.txt
 done
 
 # sort -u matches.txt | wc -l
