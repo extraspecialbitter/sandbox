93f940695aada89b8f5d8737fdff85fe1b81ad44
Paul David Mena <paul@extraspecialbitter.com>
2013-09-27 09:45:09 -0400

    update promoted apps

--- promoted_apps.sh
+++ promoted_apps.sh
@@ -5,11 +5,11 @@ export search_text=`date +%Y%m%d`
 rm -f matches.txt
 rm -f results.txt
 
-./search_for_recent_baselines.rb 20130926
+./search_for_recent_baselines.rb 20130927
 
 for i in `cat results.txt | cut -d',' -f1 | cut -d'"' -f2 | sort -u`
 do 
-  grep $i results.txt | grep 20130926 >> matches.txt
+  grep $i results.txt | grep 20130927 >> matches.txt
 done
 
 # sort -u matches.txt | wc -l
