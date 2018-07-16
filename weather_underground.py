6b801532f18b9cbcf912104a72e7a0dd4e951a38
Paul David Mena <pmena@constantcontact.com>
2018-07-16 13:36:20 -0400

    update Wayland URL

--- weather_underground.py
+++ weather_underground.py
@@ -43,8 +43,8 @@ elif len(sys.argv) == 2:
         url_forecast = urlbase + 'geolookup/conditions/forecast/q/zmw:34101.1.99999.json'
         url_sunrise  = urlbase + 'astronomy/q/zmw:34101.1.99999.json'
     elif station == 'way':
-        url_forecast = urlbase + 'geolookup/conditions/forecast/q/pws:KMAWAYLA19.json'
-        url_sunrise  = urlbase + 'astronomy/q/pws:KMAWAYLA19.json'
+        url_forecast = urlbase + 'geolookup/conditions/forecast/q/pws:KMAWAYLA13.json'
+        url_sunrise  = urlbase + 'astronomy/q/pws:KMAWAYLA13.json'
     elif station == 'cc':
         url_forecast = urlbase + 'geolookup/conditions/forecast/q/zmw:02632.1.99999.json'
         url_sunrise  = urlbase + 'astronomy/q/zmw:02632.1.99999.json'
