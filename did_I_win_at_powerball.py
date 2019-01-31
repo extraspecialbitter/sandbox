b81db2a525f54c912e632c99bcd5228a0022b78c
Paul David Mena <pmena@constantcontact.com>
2019-01-31 08:15:00 -0500

    two sets of numbers

--- did_I_win_at_powerball.py
+++ did_I_win_at_powerball.py
@@ -16,8 +16,9 @@ import csv
 
 pburl = 'https://www.powerball.com/api/v1/numbers/powerball/recent10?_format=json'
 
-my_numbers = [1, 8, 16, 28, 44, 8]
-my_powerball = my_numbers[5]
+raw_numbers = [1, 8, 16, 28, 44, 8]
+my_numbers = raw_numbers[0:5]
+my_powerball = raw_numbers[5]
 
 # read in the JSON data
 
