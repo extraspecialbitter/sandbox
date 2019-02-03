06843745669feb3f5fe6b8f02ea24c693787a254
Paul David Mena <paul@extraspecialbitter.com>
2019-02-03 11:14:45 -0500

    additional logic to deliver the bad news

--- did_I_win_at_powerball.py
+++ did_I_win_at_powerball.py
@@ -19,6 +19,7 @@ pburl = 'https://www.powerball.com/api/v1/numbers/powerball/recent10?_format=jso
 my_raw_numbers = [1, 8, 16, 28, 44, 8]
 my_numbers = my_raw_numbers[0:5]
 my_powerball = my_raw_numbers[5]
+matches = 0
 
 # read in the JSON data
 
@@ -56,6 +57,9 @@ for i in winning_numbers:
         int_me = int(i)
 	if int_me == my_numbers[j]:
            print "%s is a match" % (i)
+           matches += 1
+if matches == 0:
+    print "\nSorry - no matches"
 
 # how about the power ball?
 
