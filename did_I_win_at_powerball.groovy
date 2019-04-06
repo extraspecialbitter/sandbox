804eaae19f3e9425d00fea004e5f8c82a418d04b
Paul David Mena <pauldavidmena@gmail.com>
2019-04-06 17:58:12 -0400

    how do you like that syntax?

--- did_I_win_at_powerball.groovy
+++ did_I_win_at_powerball.groovy
@@ -1,1 +1,9 @@
-println "\nHello World!\n"
+pburl = 'https://www.powerball.com/api/v1/numbers/powerball/recent10?_format=json'
+
+my_raw_numbers = [1, 8, 16, 28, 44, 8]
+my_numbers = my_raw_numbers[0:5]
+my_powerball = my_raw_numbers[5]
+matches = 0
+pb_matches = 0
+
+
