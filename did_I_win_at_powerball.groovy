4e39ced60b6c7c6875f582de5bfbe3f35f901524
Paul David Mena <pauldavidmena@gmail.com>
2019-04-06 18:33:39 -0400

    array stuff in Java

--- did_I_win_at_powerball.groovy
+++ did_I_win_at_powerball.groovy
@@ -1,8 +1,8 @@
 pburl = 'https://www.powerball.com/api/v1/numbers/powerball/recent10?_format=json'
 
-my_raw_numbers = [1, 8, 16, 28, 44, 8]
-my_numbers = my_raw_numbers[0:5]
-my_powerball = my_raw_numbers[5]
+int[] my_raw_numbers = {1, 8, 16, 28, 44, 8}
+int[] my_numbers = Arrays.copyOfRange(my_raw_numbers, 0, 5)
+int[] my_powerball = my_raw_numbers[5]
 matches = 0
 pb_matches = 0
 
