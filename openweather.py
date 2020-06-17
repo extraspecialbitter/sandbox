e34362f1662d8b797b829c29beca1878a73d2b2f
Paul David Mena <pauldavidmena@gmail.com>
2020-06-17 17:34:48 -0400

    initial commit for script using data from openweathermap.org

---
+++ openweather.py
@@ -0,0 +1,65 @@
+#!/usr/bin/python
+
+# This script fetches the weather from the openweathermap.org API site and prints it out.
+
+from datetime import datetime, timedelta
+from sys import argv
+from os.path import join
+import urllib2
+import commands, os, socket, subprocess
+import time
+import getopt
+import sys 
+import os
+import json
+
+urlbase = 'https://api.openweathermap.org/data/2.5/weather'
+api_key = 'c4a0b83ccd51f7b1d44d35419710b2b1'
+
+# parse arguments
+
+if len(sys.argv) != 2:
+    print "\nUsage:"
+    print "%s [ mi | mx | way | cc | wh | rb | nyc ]" % sys.argv[0]
+    print "\n"
+    sys.exit(1)
+elif len(sys.argv) == 2:
+    station = sys.argv[1]
+
+    if station == 'cc':
+        city_id = '4929771'
+    elif station == 'mx':
+        city_id = '3985710'
+    else:
+        print "\nThis station feed has not been implemented yet."
+        print "\n"
+        sys.exit(1)
+
+# read in the longitude and latitude coordinates
+
+url_query = urlbase + '?id=' + city_id + '&appid=' + api_key
+f = urllib2.urlopen(url_query)
+json_string = f.read()
+parsed_json = json.loads(json_string)
+f.close()
+
+# print "\njson string: %s" % (json_string)
+# print "\nparsed json: %s" % (parsed_json)
+
+# parse for and print location
+
+city = parsed_json['name']
+country = parsed_json['sys']['country']
+print "\nCurrent weather for %s, %s:" % (city, country)
+
+# parse for weather description and temperature
+
+weather_desc = parsed_json['weather'][0]['main']
+# weather_desc2 = parsed_json['weather'][0]['description']
+kelvin_temp = parsed_json['main']['temp']
+celsius_temp = kelvin_temp - 273.15
+fahrenheit_temp = celsius_temp * ( 9 / 5 ) + 32
+
+print " %s, %s" % (weather_desc, fahrenheit_temp)
+
+
