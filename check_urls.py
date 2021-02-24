a03b4d7fcf80081e3d2e7fdbb53b9c71a8542940
Paul David Mena <pauldavidmena@gmail.com>
2021-02-24 11:15:26 -0500

    the simplest of URL tests

---
+++ check_urls.py
@@ -0,0 +1,32 @@
+#!/usr/bin/python3
+
+# This script fetches several URLs and checks for content
+
+import urllib3
+import urllib.request
+from datetime import datetime, timedelta
+from sys import argv
+from os.path import join
+import os, socket, subprocess
+import time
+import getopt
+import sys 
+import os
+import json
+
+# read in a URL
+
+url_query = 'https://ooinet.oceanobservatories.org'
+search_string = 'Research Arrays'
+
+f = urllib.request.urlopen(url_query)
+raw_string = f.read()
+f.close()
+raw_string = str(raw_string)
+
+# print ("\nraw string: %s" % (raw_string))
+
+if search_string in raw_string:
+  print ('match')
+else:
+  print ('did not find')
