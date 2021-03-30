1937f8f270b27a4a0caad8eee6b31d8be28d6995
Paul David Mena <pauldavidmena@gmail.com>
2021-03-30 15:57:56 -0400

    do only time

--- utc_conversion.py
+++ utc_conversion.py
@@ -1,4 +1,5 @@
 from datetime import datetime
 
 ts = int("1617059142")
-print(datetime.utcfromtimestamp(ts).strftime('%Y-%m-%d %H:%M:%S'))
+# print(datetime.utcfromtimestamp(ts).strftime('%Y-%m-%d %H:%M:%S'))
+print(datetime.utcfromtimestamp(ts).strftime('%H:%M:%S'))
