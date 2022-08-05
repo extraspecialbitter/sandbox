641c13683a5c50284142c551ea768528f32a1b3b
Paul David Mena <pauldavidmena@gmail.com>
2022-08-05 16:02:27 -0400

    added Garissa

--- weatherbit.py
+++ weatherbit.py
@@ -21,7 +21,7 @@ api_key = "91b2e05a9ef84a5586b4a42d49c0d7ac"
 
 if len(sys.argv) != 2:
     print ("\nUsage:")
-    print ("%s [ tx | mi | ts | gj | cdmx | scc | pr | vqs | way | cc | wh | bos | nyc | ca | ifl | vic | osu | nj | li | cam | pt ]" % sys.argv[0])
+    print ("%s [ tx | mi | ts | gj | cdmx | scc | pr | vqs | way | cc | wh | bos | nyc | ca | ifl | vic | osu | nj | li | cam | pt | gar ]" % sys.argv[0])
     print ("\n")
     sys.exit(1)
 elif len(sys.argv) == 2:
@@ -93,6 +93,9 @@ elif len(sys.argv) == 2:
     elif station == 'pt':
         lat = 42.0547
         lon = -70.1846
+    elif station == 'gar':
+        lat = 0.4532
+        lon = 39.6461
     else:
         print ("\nThis station feed has not been implemented yet.")
         print ("\n")
