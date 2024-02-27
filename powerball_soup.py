c5c832db45881313a656ddb1d3cdea799f18e3d0
Paul David Mena <pauldavidmena@gmail.com>
2024-02-27 10:18:27 -0500

    trying to get drawing date as well

--- powerball_soup.py
+++ powerball_soup.py
@@ -19,8 +19,17 @@ def get_powerball_jackpot():
         # Extract the jackpot amount
         jackpot_amount = jackpot_element.find('span', class_='game-jackpot-number').text.strip()
 
+	# Find the element containing the drawing information
+        drawing_element = soup.find('div', class_='next-card')
+
+        # Extract the next drawing date
+#       drawing_date = jackpot_element.find('h5', class_='title-date').text.strip()
+
         # Print the jackpot amount
         print("Latest Powerball Jackpot:", jackpot_amount)
+
+        # Print the drawing date
+#       print("Next Drawing Date:", drawing_date)
     else:
         print("Failed to retrieve Powerball jackpot information.")
 
